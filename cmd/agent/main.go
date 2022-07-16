ee5d2898bc390204ba69ebd00f0ba5559707a612
Marsel Abdrakhmanov <dev.marsel@sigmasms.ru>
2022-07-16 19:28:45 +0300

    Change agent updating stats

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -88,15 +88,99 @@ func main() {
 	wg.Add(1)
 	go UpdateMetrics(cfg.PoolInterval, wg, cache)
 
-	wg.Add(1)
-	go UpdateExtraMetrics(cfg.PoolInterval, wg, cache)
+	//wg.Add(1)
+	//go UpdateExtraMetrics(cfg.PoolInterval, wg, cache)
 
 	wg.Add(1)
 	go SendMetrics(cfg, wg, cache)
 
-	//http.ListenAndServe("localhost:8080", nil)
+	http.ListenAndServe("localhost:8080", nil)
 
 	wg.Wait()
+}
+
+func UpdateMetrics(interval time.Duration, wg *sync.WaitGroup, cache *AgentCache) {
+	defer wg.Done()
+
+	var pollCount int
+
+	rand.Seed(time.Now().Unix())
+
+	ticker := time.NewTicker(interval)
+	for range ticker.C {
+		log.Println("Update metrics...")
+		pollCount++
+		cache.Set(PollCount, NewCounterMetric(PollCount, int64(pollCount)))
+		cache.Set(RandomValue, NewGaugeMetric(RandomValue, float64(rand.Intn(10000))))
+		SetRuntimeStats(cache)
+
+		log.Println("UpdateExtraMetrics...")
+		SetVirtualMemoryStats(cache)
+		SetCpuStats(cache)
+	}
+}
+
+func SetRuntimeStats(cache *AgentCache) {
+	var memStats runtime.MemStats
+	runtime.ReadMemStats(&memStats)
+
+	cache.Set(Alloc, NewGaugeMetric(Alloc, float64(memStats.Alloc)))
+	cache.Set(BuckHashSys, NewGaugeMetric(BuckHashSys, float64(memStats.BuckHashSys)))
+	cache.Set(Frees, NewGaugeMetric(Frees, float64(memStats.Frees)))
+	cache.Set(GCCPUFraction, NewGaugeMetric(GCCPUFraction, memStats.GCCPUFraction))
+	cache.Set(GCSys, NewGaugeMetric(GCSys, float64(memStats.GCSys)))
+	cache.Set(HeapAlloc, NewGaugeMetric(HeapAlloc, float64(memStats.HeapAlloc)))
+	cache.Set(HeapIdle, NewGaugeMetric(HeapIdle, float64(memStats.HeapIdle)))
+	cache.Set(HeapInuse, NewGaugeMetric(HeapInuse, float64(memStats.HeapInuse)))
+	cache.Set(HeapObjects, NewGaugeMetric(HeapObjects, float64(memStats.HeapObjects)))
+	cache.Set(HeapReleased, NewGaugeMetric(HeapReleased, float64(memStats.HeapReleased)))
+	cache.Set(HeapSys, NewGaugeMetric(HeapSys, float64(memStats.HeapSys)))
+	cache.Set(LastGC, NewGaugeMetric(LastGC, float64(memStats.LastGC)))
+	cache.Set(Lookups, NewGaugeMetric(Lookups, float64(memStats.Lookups)))
+	cache.Set(MCacheSys, NewGaugeMetric(MCacheSys, float64(memStats.MCacheSys)))
+	cache.Set(MCacheInuse, NewGaugeMetric(MCacheInuse, float64(memStats.MCacheInuse)))
+	cache.Set(MSpanInuse, NewGaugeMetric(MSpanInuse, float64(memStats.MSpanInuse)))
+	cache.Set(MSpanSys, NewGaugeMetric(MSpanSys, float64(memStats.MSpanSys)))
+	cache.Set(Mallocs, NewGaugeMetric(Mallocs, float64(memStats.Mallocs)))
+	cache.Set(NextGC, NewGaugeMetric(NextGC, float64(memStats.NextGC)))
+	cache.Set(NumForcedGC, NewGaugeMetric(NumForcedGC, float64(memStats.NumForcedGC)))
+	cache.Set(NumGC, NewGaugeMetric(NumGC, float64(memStats.NumGC)))
+	cache.Set(OtherSys, NewGaugeMetric(OtherSys, float64(memStats.OtherSys)))
+	cache.Set(PauseTotalNs, NewGaugeMetric(PauseTotalNs, float64(memStats.PauseTotalNs)))
+	cache.Set(StackInuse, NewGaugeMetric(StackInuse, float64(memStats.StackInuse)))
+	cache.Set(StackSys, NewGaugeMetric(StackSys, float64(memStats.StackSys)))
+	cache.Set(TotalAlloc, NewGaugeMetric(TotalAlloc, float64(memStats.TotalAlloc)))
+	cache.Set(Sys, NewGaugeMetric(Sys, float64(memStats.Sys)))
+}
+
+func UpdateExtraMetrics(interval time.Duration, wg *sync.WaitGroup, cache *AgentCache) {
+	defer wg.Done()
+
+	ticker := time.NewTicker(interval)
+	for range ticker.C {
+		log.Println("UpdateExtraMetrics...")
+
+		SetVirtualMemoryStats(cache)
+
+		SetCpuStats(cache)
+	}
+}
+
+func SetVirtualMemoryStats(cache *AgentCache) {
+	v, err := mem.VirtualMemory()
+	if err != nil {
+		log.Println(err)
+	}
+	cache.Set(TotalMemory, NewGaugeMetric(TotalMemory, float64(v.Total)))
+	cache.Set(FreeMemory, NewGaugeMetric(FreeMemory, float64(v.Free)))
+}
+
+func SetCpuStats(cache *AgentCache) {
+	percent, err := cpu.Percent(time.Second, false)
+	if err != nil {
+		log.Println(err)
+	}
+	cache.Set(CPUutilization1, NewGaugeMetric(CPUutilization1, percent[0]))
 }
 
 func SendMetrics(cfg *config.AgentConfig, wg *sync.WaitGroup, cache *AgentCache) {
@@ -131,76 +215,5 @@ func SendMetrics(cfg *config.AgentConfig, wg *sync.WaitGroup, cache *AgentCache)
 			log.Println(err)
 			break
 		}
-	}
-}
-
-func UpdateExtraMetrics(interval time.Duration, wg *sync.WaitGroup, cache *AgentCache) {
-	defer wg.Done()
-
-	ticker := time.NewTicker(interval)
-	for range ticker.C {
-		log.Println("UpdateExtraMetrics...")
-
-		v, err := mem.VirtualMemory()
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		cache.Set(TotalMemory, NewGaugeMetric(TotalMemory, float64(v.Total)))
-		cache.Set(FreeMemory, NewGaugeMetric(FreeMemory, float64(v.Free)))
-
-		percent, err := cpu.Percent(time.Second, false)
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		cache.Set(CPUutilization1, NewGaugeMetric(CPUutilization1, percent[0]))
-	}
-}
-
-func UpdateMetrics(interval time.Duration, wg *sync.WaitGroup, cache *AgentCache) {
-	defer wg.Done()
-
-	var memStats runtime.MemStats
-	var pollCount int
-
-	rand.Seed(time.Now().Unix())
-
-	ticker := time.NewTicker(interval)
-	for range ticker.C {
-		log.Println("Update metrics...")
-
-		runtime.ReadMemStats(&memStats)
-		pollCount++
-
-		cache.Set(Alloc, NewGaugeMetric(Alloc, float64(memStats.Alloc)))
-		cache.Set(BuckHashSys, NewGaugeMetric(BuckHashSys, float64(memStats.BuckHashSys)))
-		cache.Set(Frees, NewGaugeMetric(Frees, float64(memStats.Frees)))
-		cache.Set(GCCPUFraction, NewGaugeMetric(GCCPUFraction, memStats.GCCPUFraction))
-		cache.Set(GCSys, NewGaugeMetric(GCSys, float64(memStats.GCSys)))
-		cache.Set(HeapAlloc, NewGaugeMetric(HeapAlloc, float64(memStats.HeapAlloc)))
-		cache.Set(HeapIdle, NewGaugeMetric(HeapIdle, float64(memStats.HeapIdle)))
-		cache.Set(HeapInuse, NewGaugeMetric(HeapInuse, float64(memStats.HeapInuse)))
-		cache.Set(HeapObjects, NewGaugeMetric(HeapObjects, float64(memStats.HeapObjects)))
-		cache.Set(HeapReleased, NewGaugeMetric(HeapReleased, float64(memStats.HeapReleased)))
-		cache.Set(HeapSys, NewGaugeMetric(HeapSys, float64(memStats.HeapSys)))
-		cache.Set(LastGC, NewGaugeMetric(LastGC, float64(memStats.LastGC)))
-		cache.Set(Lookups, NewGaugeMetric(Lookups, float64(memStats.Lookups)))
-		cache.Set(MCacheSys, NewGaugeMetric(MCacheSys, float64(memStats.MCacheSys)))
-		cache.Set(MCacheInuse, NewGaugeMetric(MCacheInuse, float64(memStats.MCacheInuse)))
-		cache.Set(MSpanInuse, NewGaugeMetric(MSpanInuse, float64(memStats.MSpanInuse)))
-		cache.Set(MSpanSys, NewGaugeMetric(MSpanSys, float64(memStats.MSpanSys)))
-		cache.Set(Mallocs, NewGaugeMetric(Mallocs, float64(memStats.Mallocs)))
-		cache.Set(NextGC, NewGaugeMetric(NextGC, float64(memStats.NextGC)))
-		cache.Set(NumForcedGC, NewGaugeMetric(NumForcedGC, float64(memStats.NumForcedGC)))
-		cache.Set(NumGC, NewGaugeMetric(NumGC, float64(memStats.NumGC)))
-		cache.Set(OtherSys, NewGaugeMetric(OtherSys, float64(memStats.OtherSys)))
-		cache.Set(PauseTotalNs, NewGaugeMetric(PauseTotalNs, float64(memStats.PauseTotalNs)))
-		cache.Set(StackInuse, NewGaugeMetric(StackInuse, float64(memStats.StackInuse)))
-		cache.Set(StackSys, NewGaugeMetric(StackSys, float64(memStats.StackSys)))
-		cache.Set(TotalAlloc, NewGaugeMetric(TotalAlloc, float64(memStats.TotalAlloc)))
-		cache.Set(Sys, NewGaugeMetric(Sys, float64(memStats.Sys)))
-		cache.Set(PollCount, NewCounterMetric(PollCount, int64(pollCount)))
-		cache.Set(RandomValue, NewGaugeMetric(RandomValue, float64(rand.Intn(10000))))
 	}
 }
